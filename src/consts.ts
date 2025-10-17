78dd16e48b02d9a7cb499ff3a4cfc46714f7fa47
xxxTristantacion <tmonat-merlen@gaming.tech>
2025-10-17 22:41:24 +0200

    buildable, Error on linking images

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Portfolio';
-export const SITE_DESCRIPTION = 'Welcome the portfolio of Tristan MONAT--MERLEN!';
+export const SITE_DESCRIPTION = 'Welcome the portfolio of Tristan MONAT--MERLEN !';
+
+// TODO : Change this dynamically
+export const SITE_ROOT = "D:/Project/Portfolio/lonely-limb/dist/";
